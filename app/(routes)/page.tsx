9924ea4f9e568d26c944df1c97aef6ad309de15e
Wanderson Castro <wandersonce1@hotmail.com>
2023-08-24 13:12:38 -0700

    Fetching test Billboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,13 +1,15 @@
+import { getBillboard } from "@/actions/getBillboard";
 import Billboard from "@/components/Billboard";
 import Container from "@/components/ui/Container";
 
 const revalidate = 0;
 
-export default function HomePage() {
+export default async function HomePage() {
+  const billboard = await getBillboard("1d93eeb3-2a54-40dd-a0d4-3c0f8e94e7f6");
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard data={}/>
+        <Billboard data={billboard}/>
       </div>
     </Container>
   )

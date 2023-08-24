7aa0a1cf9bd437f2dc470ec770a3c9e5cef37072
Wanderson Castro <wandersonce1@hotmail.com>
2023-08-24 13:45:26 -0700

    Fetching Products and passing to Product List

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,15 +1,21 @@
 import { getBillboard } from "@/actions/getBillboard";
+import { getProducts } from "@/actions/getProducts";
 import Billboard from "@/components/Billboard";
+import ProductList from "@/components/ProductList";
 import Container from "@/components/ui/Container";
 
 const revalidate = 0;
 
 export default async function HomePage() {
+  const products = await getProducts({isFeatured:true});
   const billboard = await getBillboard("1d93eeb3-2a54-40dd-a0d4-3c0f8e94e7f6");
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard}/>
+      </div>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductList title="Featured Products" items={products} />
       </div>
     </Container>
   )
---
+++ components/ProductList.tsx
@@ -0,0 +1,16 @@
+import { Product } from "@/types";
+
+interface ProductListProps{
+  title: string;
+  items: Product[];
+}
+
+export default function ProductList({title, items} : ProductListProps) {
+  return (
+    <div className="space-y-4">
+      <h3 className="font-bold text-3xl">
+        {title} 
+      </h3>
+    </div>
+  )
+}

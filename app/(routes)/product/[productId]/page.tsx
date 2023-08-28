e97b611278ff59ebfeae8151a568e72ad4fd7ed3
Wanderson Castro <wandersonce1@hotmail.com>
2023-08-28 13:02:18 -0700

    Creating gallery Grid

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -1,5 +1,8 @@
 import { getProduct } from "@/actions/getProduct";
 import { getProducts } from "@/actions/getProducts";
+import ProductList from "@/components/ProductList";
+import Gallery from "@/components/gallery";
+import Container from "@/components/ui/Container";
 
 interface ProductPageProps{
   params: {
@@ -13,8 +16,21 @@ export default async function ProductPage({params} : ProductPageProps) {
   })
 
   return (
-    <div>
-      
+    <div className="bg-white">
+      <Container>
+        <div className="px-4 py-10 sm:px-6 lg:px-8">
+          <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+            {/* Gallery */}
+            <Gallery images={product.images} />
+            <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
+              {/* Info */}
+              Info
+            </div>
+          </div>
+          <hr className="my-10" />
+          <ProductList title="Related Items" items={suggestedProducts} />
+        </div>
+      </Container>
     </div>
   )
 }

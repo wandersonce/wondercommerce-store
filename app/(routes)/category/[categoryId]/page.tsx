f7112a69957725fc9544f4f7931ba52f0535271b
Wanderson Castro <wandersonce1@hotmail.com>
2023-08-29 12:27:31 -0700

    Adding product card rendering

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -5,6 +5,8 @@ import { getSizes } from "@/actions/getSizes";
 import Billboard from "@/components/Billboard";
 import Container from "@/components/ui/Container";
 import Filter from "./components/Filter";
+import NoResults from "@/components/ui/NoResults";
+import ProductCard from "@/components/ui/ProductCard";
 
 export const revalidate = 0;
 
@@ -38,6 +40,15 @@ export default async function CategoryPage({params, searchParams} : CategoryPage
             {/* Mobile Filters */}
             <div className="hidden lg:block">
               <Filter valueKey="sizeId" name="Sizes" data={sizes} />
+              <Filter valueKey="colorId" name="Colors" data={colors} />
+            </div>
+            <div className="mt-6 lg:col-span-4 lg:mt-0">
+              {products.length === 0 && <NoResults />}
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                {products.map((item)=> (
+                  <ProductCard  key={item.id} data={item}/>
+                ))}
+              </div>
             </div>
           </div>
         </div>
